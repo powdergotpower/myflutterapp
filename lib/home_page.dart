c8cc333ba11fcdfd129c931512e965a227a55771
powdergotpower <anshjotsinghsidhu@gmail.com>
2026-02-12 10:50:19 +0530

    Create home_page.dart

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bubble Game"),
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to Bubble Pop Game",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
